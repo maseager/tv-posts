878539446f585fa0160c280700c9e57339ff57b7
mseager <130193727+maseager@users.noreply.github.com>
2025-06-18 04:06:51 -0400

    Updated WelcomeSection.tsx

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -14,7 +14,7 @@ const WelcomeSection: React.FC = () => {
           Login
         </button>
         <button className="px-8 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-medium transition-colors">
-          Register Free
+          Register
         </button>
       </div>
     </div>
