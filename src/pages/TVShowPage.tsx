80c0bc67776d4608304eae1c590234dc35680e29
mseager <130193727+maseager@users.noreply.github.com>
2025-06-21 14:32:10 -0400

    Fix watchlist button alert positioning

--- src/pages/TVShowPage.tsx
+++ src/pages/TVShowPage.tsx
@@ -175,7 +175,7 @@ const TVShowPage: React.FC = () => {
 
       {/* Login Prompt */}
       {showLoginPrompt && (
-        <div className="fixed top-24 left-1/2 transform -translate-x-1/2 bg-gray-800 border border-gray-600 rounded-lg p-4 shadow-lg z-50 animate-fade-in">
+        <div className="fixed top-24 left-1/2 -translate-x-1/2 bg-gray-800 border border-gray-600 rounded-lg p-4 shadow-lg z-50 animate-fade-in">
           <div className="flex items-center space-x-2 text-[#77d4fc]">
             <AlertCircle className="w-5 h-5" />
             <span className="font-medium">Log in to add this show - your watchlist is waiting</span>
--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -209,7 +209,7 @@ const UserProfilePage: React.FC = () => {
 
       {/* Login Prompt */}
       {showLoginPrompt && (
-        <div className="fixed top-24 left-1/2 transform -translate-x-1/2 bg-gray-800 border border-gray-600 rounded-lg p-4 shadow-lg z-50 animate-fade-in">
+        <div className="fixed top-24 left-1/2 -translate-x-1/2 bg-gray-800 border border-gray-600 rounded-lg p-4 shadow-lg z-50 animate-fade-in">
           <div className="flex items-center space-x-2 text-[#77d4fc]">
             <AlertCircle className="w-5 h-5" />
             <span className="font-medium">Log in to follow your favorite TV fans</span>
